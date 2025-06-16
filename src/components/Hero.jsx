f014ed98463fff413a7ef1c3e20abb480848157b
Karthik Yadav <mkarthikyadav2004@gmail.com>
2025-06-16 21:36:53 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
         <img
           src={mobileimage}
           alt='Mobile Visual'
-         className='absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-auto object-contain'
+         className='absolute bottom-10 left-1/2 transform -translate-x-1/2 w-[80%] h-auto object-contain'
     style={{ filter: "drop-shadow(0 0 10px #915EFF)" }}
         />
       ) : (
